698ffea249333682dd048546dbb0d1df2696f1e2
girogio <giorgiogrigolo03@gmail.com>
2024-03-02 00:26:28 +0100

    Renamed Category generic identifier to Alpha

--- src/utils/dfsa.rs
+++ src/utils/dfsa.rs
@@ -1,18 +1,18 @@
 use std::marker::PhantomData;
 
-pub struct Dfsa<State, Category: Sized, Delta>
+pub struct Dfsa<State, Alpha: Sized, Delta>
 where
     State: PartialEq + Eq,
-    Delta: Fn(State, Category) -> State,
+    Delta: Fn(State, Alpha) -> State,
 {
     accepted_states: Vec<State>,
-    category: PhantomData<Category>,
+    category: PhantomData<Alpha>,
     start_state: State,
     delta: Delta,
 }
 
-impl<State: PartialEq + Eq + Copy, Category, Delta: Fn(State, Category) -> State>
-    Dfsa<State, Category, Delta>
+impl<State: PartialEq + Eq + Copy, Alpha, Delta: Fn(State, Alpha) -> State>
+    Dfsa<State, Alpha, Delta>
 {
     pub fn new(accepted_states: Vec<State>, start_state: State, delta: Delta) -> Self {
         Dfsa {
@@ -31,7 +31,7 @@ impl<State: PartialEq + Eq + Copy, Category, Delta: Fn(State, Category) -> State
         self.accepted_states.contains(state)
     }
 
-    pub fn delta(&self, state: State, category: Category) -> State {
+    pub fn delta(&self, state: State, category: Alpha) -> State {
         (self.delta)(state, category)
     }
 }
