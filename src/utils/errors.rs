de1687ddd37ee64f2a986de4040084f6fe189f6b
girogio <giorgiogrigolo03@gmail.com>
2024-03-05 00:14:14 +0100

    Added Parse Error type

--- src/utils/errors.rs
+++ src/utils/errors.rs
@@ -1,11 +1,13 @@
 use thiserror::Error;
 
-use crate::core::TextSpan;
+use crate::core::{TextSpan, Token};
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("Lexical error: {0}")]
     Lexical(#[from] LexicalError),
+    #[error("Parse error: {0}")]
+    Parse(#[from] ParseError),
 }
 
 #[derive(Debug, Error)]
@@ -18,6 +20,12 @@ pub enum LexicalError {
     InvalidFloatLiteral(TextSpan),
     #[error("Unterminated block comment.")]
     UnterminatedBlockComment(TextSpan),
+}
+
+#[derive(Debug, Error)]
+pub enum ParseError {
+    #[error("Unexpected token: {0:?}")]
+    UnexpectedToken(Token),
 }
 
 // pub type Result<T> = std::result::Result<T, Error>;
