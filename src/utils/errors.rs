4f04cc7ae09a24bf0d7dd0ef13bd6fa87288bb19
girogio <giorgiogrigolo03@gmail.com>
2024-03-20 19:44:06 +0100

    Added source hyperlinks to parsing error outputs

--- src/utils/errors.rs
+++ src/utils/errors.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use thiserror::Error;
 
 use crate::core::{TextSpan, Token, TokenKind};
@@ -24,26 +26,75 @@ pub enum LexicalError {
 
 #[derive(Debug, Error)]
 pub enum ParseError {
-    #[error("Parse error in file: {file}:{line}:{col}\nUnexpected token: expected {expected:?}, found {found:?}")]
+    #[error("Parse error in file: {file}:{line}:{col}\nUnexpected token found at {}:{}:{} \nExpected {expected:?}, found {found}", .source_file.display(), .found.span.from_line, .found.span.from_col)]
     UnexpectedToken {
         expected: TokenKind,
         found: Token,
+        source_file: PathBuf,
         file: &'static str,
         line: u32,
         col: u32,
     },
-    #[error("Parse error in file: {}:{}:{}\nUnexpected token: {:?} expected one of these types: {:?}",.file, .line, .col, .found, .expected)]
+    #[error("Parser error in file: {}:{}:{}\nUnexpected token found at {}:{}:{} \nExpected one of these types: {:?}",.file, .line, .col, .source_file.display(), .found.span.from_line, .found.span.from_col, .expected)]
     UnexpectedTokenList {
         file: &'static str,
         line: u32,
         col: u32,
+        source_file: PathBuf,
         found: Token,
         expected: Vec<TokenKind>,
     },
     #[error("Unclosed block.")]
     UnclosedBlock,
-    #[error("Unexpected end of file.")]
-    UnexpectedEof,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::core::TokenKind;
+
+    #[test]
+    fn test_lexical_error() {
+        let error = LexicalError::InvalidCharacter(TextSpan::new(1, 1, 1, 1, "a"));
+        assert_eq!(
+            format!("{}", error),
+            "Unrecognized character 'a' found at 1:1"
+        );
+
+        let error = LexicalError::UnterminatedString(TextSpan::new(1, 1, 1, 1, "a"));
+        assert_eq!(format!("{}", error), "Unterminated string.");
+
+        let error = LexicalError::InvalidFloatLiteral(TextSpan::new(1, 1, 1, 1, "a"));
+        assert_eq!(format!("{}", error), "Invalid float literal ending at 1:1");
+
+        let error = LexicalError::UnterminatedBlockComment(TextSpan::new(1, 1, 1, 1, "a"));
+        assert_eq!(format!("{}", error), "Unterminated block comment.");
+    }
+
+    #[test]
+    fn test_parse_error() {
+        let error = ParseError::UnexpectedToken {
+            expected: TokenKind::Identifier,
+            found: Token::new(TokenKind::IntLiteral, TextSpan::new(1, 1, 1, 1, "1")),
+            file: "file",
+            source_file: PathBuf::from("source_file"),
+            line: 1,
+            col: 1,
+        };
+        assert_eq!(
+            format!("{}", error),
+            "Parse error in file: file:1:1\nUnexpected token: expected Identifier, found Token { kind: IntLiteral, lexeme: \"1\", span: TextSpan { from_line: 1, from_col: 1, to_line: 1, to_col: 1 } }"
+        );
+
+        let error = ParseError::UnexpectedTokenList {
+            file: "file",
+            source_file: "source_file",
+            line: 1,
+            col: 1,
+            found: Token::new(TokenKind::IntLiteral, TextSpan::new(1, 1, 1, 1, "1")),
+            expected: vec![TokenKind::Identifier, TokenKind::FloatLiteral],
+        };
+    }
+}
