5134e60455e09bf76ea247cf4372412f4dd4438c
girogio <giorgiogrigolo03@gmail.com>
2024-03-23 19:50:41 +0100

    Manual implementation of Debug display for Token

--- src/core/tokens.rs
+++ src/core/tokens.rs
@@ -1,9 +1,10 @@
+use core::fmt::Debug;
 use std::fmt::Display;
 
 use crate::core::TextSpan;
 use crate::core::TokenKind;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Token {
     pub kind: TokenKind,
     pub span: TextSpan,
@@ -18,5 +19,11 @@ impl Token {
 impl Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "{}", self.span.lexeme)
+    }
+}
+
+impl Debug for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self.kind)
     }
 }
